e3bce643c792bd8d86a0bf21aac23a49cdc22ec9
wahu0001 <max513002011@hotmail.fr>
2021-11-03 18:47:08 +0100

    while game is not over function

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from Helper.input import prompt
 def main():
     grid_height = prompt('Veuillez entrer la hauteur de la grille (entre 5 et 15) : ')
     grid_width = prompt('Veuillez entrer la largeur de la grille (entre 5 et 15) : ')
+    play()
     print(grid_height)
     print(grid_width)
 
 if __name__ == '__main__':
     main()
+
+
+def play():
+    game_over = False
+    while not game_over :
+        askAction()
+
+
+def askAction():
+    return prompt('Entrez une command (help pour la liste des commandes)')
+    #doCommad(prompt('Entrez une command (help pour la liste des commandes)'))
