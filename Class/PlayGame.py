3b84c52e41562ee3e09da78f2ed2a22820d406d5
Samy QAIS <samy.qais.dev@gmail.com>
2021-11-19 16:12:41 +0100

    4.2 Fait - Exercice 4 à commencer

--- Class/PlayGame.py
+++ Class/PlayGame.py
@@ -1,3 +1,15 @@
+from Class.MineSweeper import MineSweeper
+from Class.Player import Player
+from Helper.Command import Command
+
 class PlayGame:
     
-    # def __init__(self):
+    def __init__(self, mine_sweepper: MineSweeper, player: Player):
+        self.command = Command()
+        self.mine_sweepper = mine_sweepper
+        self.player = player
+
+    def run(self):
+        while not self.mine_sweepper.game_over :
+            print(str(self.mine_sweepper.grid))
+            answer = self.command.askAction('Entrez une commande (help pour la liste des commandes) : ', self)
